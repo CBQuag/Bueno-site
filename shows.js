2c4175cd4f588b38ff9863515fc7b3421d912209
Conrad Quagliaroli <conradbquag@gmail.com>
2023-04-13 13:55:00 -0400

    doing shows

--- shows.js
+++ shows.js
@@ -1,6 +1,20 @@
 const express=require('express');
 const router=express.Router();
 
+const showList=[
+    {
+        name:
+    },
+    {
 
+    },
+    {
+
+    }
+]
+
+router.get('/',(req,res)=>{    
+    res.send()
+});
 
 module.exports = router;
--- users.js
+++ users.js
@@ -1,6 +1,20 @@
 const express=require('express');
 const router=express.Router();
 
+router.get('/',(req,res)=>{    
+    res.send()
+});
 
+router.post('/',(req,res)=>{    
+    res.send()
+});
+
+router.pull('/',(req,res)=>{    
+    res.send()
+});
+
+router.delete('/',(req,res)=>{    
+    res.send()
+});
 
 module.exports = router;
