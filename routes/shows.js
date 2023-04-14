0500c077694fd26ed2fc2f7acdc02d51e889a4d6
Conrad Quagliaroli <conradbquag@gmail.com>
2023-04-14 10:18:32 -0400

    legibility improvements

--- routes/shows.js
+++ routes/shows.js
@@ -13,6 +13,7 @@ router.get('/',(req,res)=>{
             res.status(500).send('There was a problem reading the file.')
             return;
         }
+
         console.log('Shows')
         res.json(JSON.parse(data));
     })
@@ -20,12 +21,18 @@ router.get('/',(req,res)=>{
 
 router.get('/:name', (req, res)=>{
     console.log('Finding specific show: ')
+
     const {name}=req.params;
+
     console.log(name);
+
     let jsonShow=JSON.parse(showList.toString())
     currentShow=jsonShow.find(show=>show.name.toLowerCase()===name.toLowerCase());
+
     console.log('Current Show: ',currentShow)
+    
     res.send(`<h1>${currentShow.name} (${currentShow.date.year})</h1>
               <a style='text-decoration:none' href='./'>Back to main</a>`)
 })
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const fs=require('fs')
 
 let USER_FILE='./data/users.json';
-// let USER_LISTS='./data/user-lists.json';
+let USER_LISTS='./data/user-lists.json';
 
 router.get('/',(req,res)=>{    
     fs.readFile(USER_FILE, 'utf-8', (err, data)=>{
@@ -13,27 +13,30 @@ router.get('/',(req,res)=>{
             return;
         }
         res.json(JSON.parse(data));
-});
+    })
+})
 
-router.post('/',(req,res)=>{  
+router.post('/',(req,res)=>{
+
+    //posts a new user to the list of users
     fs.readFile(USER_FILE, 'utf-8', (err, data)=>{
         if(err){
             console.error(err);
             res.status(500).send('There was a problem reading the file.')
             return;
         }
-        const userList=JSON.parse(data);
+        const userFile=JSON.parse(data);
 
         const newUser={
             name:req.body.name,
             pass:req.body.pass
         };
         
-        userList.push(newUser);
+        userFile.push(newUser);
 
-        console.log(userList);
+        console.log(userFile);
 
-        fs.writeFile(USER_FILE, JSON.stringify(userList), err=>{
+        fs.writeFile(USER_FILE, JSON.stringify(userFile), err=>{
             if(err){
                 console.error(err);
                 res.status(500).send('There was a problem reading the file.')
@@ -41,11 +44,36 @@ router.post('/',(req,res)=>{
             }
             res.json(newUser);
         })
-
-        //post a new list here
-
     })
-    res.send()
+
+    //posts a new list for the new user
+    fs.readFile(USER_LISTS, 'utf-8', (err, data)=>{
+        if(err){
+            console.error(err);
+            res.status(500).send('There was a problem reading the file.')
+            return;
+        }
+
+        const userList=JSON.parse(data);
+
+        let newList={
+            "user":req.body.name,
+            "list":[]
+        }
+
+        userList.push(newList);
+
+        console.log(newList);
+
+        fs.writeFile(USER_LISTS, JSON.stringify(userList), err=>{
+            if(err){
+                console.error(err);
+                res.status(500).send('There was a problem reading the file.')
+                return;
+            }
+            res.json(newList);
+        })
+    })
 });
 
 router.put('/',(req,res)=>{    
