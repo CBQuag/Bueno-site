76b9a58ead1da7f6f141886541f5c8c5cb59a2e3
Conrad Quagliaroli <conradbquag@gmail.com>
2023-05-01 10:04:32 -0400

    changed port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port=3030;
+const port=process.env.PORT||3000;
 
 app.get('/',(req,res)=>{
     res.send('Welcome, welcome to buenosite');
